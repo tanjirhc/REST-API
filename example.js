2dcac3e58ad06cb0f655b8f3c594d2a9401c1de6
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-09 06:34:25 +0600

    REST API model

---
+++ example.js
@@ -0,0 +1,20 @@
+//Identify Object Model
+
+/*
+! USER
+    -name
+    -id
+    -email
+    -password
+    -birthday
+    -phoneno
+    
+*/
+
+
+// Create Model URI
+
+// Determine Representation
+// Single Data Representation
+
+// JSON View
